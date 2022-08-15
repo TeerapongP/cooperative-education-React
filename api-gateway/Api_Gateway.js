03b3abb90ba30b3d6d2d7a762bf3e7e2c36b7d54
TeerapongP <std25406@snws.ac.th>
2022-08-15 12:45:37 +0700

    no time finish

--- api-gateway/Api_Gateway.js
+++ api-gateway/Api_Gateway.js
@@ -1,11 +1,18 @@
 const express = require('express')
+const cors = require('cors')
 const tripsData = require('../json-server/db.json')
+
 const PORT = 4000
 const app = express()
+app.use(cors())
 
-app.get('/trips', (req, res) => {
-    const keyword = req.query.keyword;
-    res.json(tripsData.trips.filter(trip => trip.title.includes(keyword) | trip.description.includes(keyword) | trip.tags.includes(keyword)))
+app.get('/trips', (request, response) => {
+    const keyword = request.query.keyword;
+    if(keyword){
+        response.send(tripsData.trips.filter(trip => trip.title.includes(keyword) || trip.description.includes(keyword) || trip.tags.includes(keyword)))
+    }
+    response.send(tripsData)
+
 })
 app.listen(PORT, () => {
     console.log('Start server at port 4000.')
--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useEffect } from 'react'
+import axios from 'axios';
 
-function App() {
+import './App.css';
+export default function App() {
+  const [data,setData] = useState([])
+  
+  useEffect(() =>{
+    axios.
+    get("http://localhost:4000/trips",)
+    .then((response) => {
+      setData(response.data);
+    });
+  },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* header and search */}
+      <h1>เที่ยวไหนดี</h1>
+      <input className='seach-data' type='text' placeholder='หาที่เที่ยวแล้วไปกัน...'></input>
+      <div>
+      <hr className='horizontal-line'/>
+      </div>
+      {/* OUTPUT PROGRAM */}
+      {
+        Object.keys(data).map((keys)=>{
+          return data[keys].map((value) => 
+          {
+            return(
+              <div className='data-trips'>
+                <div className='title-trips'>
+                  <h2>{value.title}</h2>
+                </div> 
+                {/* <div className='discription-trips'>
+                  <p>{value.description}</p>
+                </div> */}
+                <div className='images-trips'>
+                    <img src={value.photos[0]}/>
+                    <img src={value.photos[1]} id='photos1'/>
+                    <img src={value.photos[2]} id='photos2'/>
+                    <img src={value.photos[3]} id='photos3'/>
+                </div>
+                {/* <div className='tags-trips'>
+                  <span>หมวด : {value.tags[0]}</span>
+                  <span id='tags1'>{value.tags[1]}</span>
+                  <span id='tags2'>{value.tags[2]}</span>
+                  <span id='tags3'>{value.tags[3]}</span>
+                </div> */}
+            </div>
+            )
+          })
+        })
+        }
+        
     </div>
   );
 }
 
-export default App;
